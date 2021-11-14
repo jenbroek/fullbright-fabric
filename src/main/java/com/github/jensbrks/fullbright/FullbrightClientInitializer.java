b2e9f16daa732d0ae1d6659664fa3e4f7f2e783d
Jens Broekens <jens.broekens@protonmail.com>
2021-11-14 16:01:58 +0100

    initial commit

---
+++ src/main/java/com/github/jensbrks/fullbright/FullbrightClientInitializer.java
@@ -0,0 +1,12 @@
+package com.github.jensbrks.fullbright;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FullbrightClientInitializer implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		// TODO
+	}
+
+}
